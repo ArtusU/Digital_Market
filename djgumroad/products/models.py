37735ffc474fdfc8ac45c3139b1f0987a948d41f
ArtusU <artusrock@hotmail.com>
2021-02-18 16:10:27 +0000

    product update and delete view

--- djgumroad/products/models.py
+++ djgumroad/products/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.signals import user_logged_in
 from django.db import models
 from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
+    user        = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='products')
     name        = models.CharField(max_length=100)
     description = models.TextField()
     cover       = models.ImageField(blank=True, null=True, upload_to="product_covers/")
@@ -18,7 +20,17 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('products:product-detail', kwargs={
-            'slug': self.slug
+            "slug": self.slug
+        })
+
+    def get_update_url(self):
+        return reverse('products:product-update', kwargs={
+            "slug": self.slug
+        })
+
+    def get_delete_url(self):
+        return reverse('products:product-delete', kwargs={
+            "slug": self.slug
         })
 
 
